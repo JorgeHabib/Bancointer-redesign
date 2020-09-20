cd26bab8e9ced25b7a0824247034af0226510c25
JorgeHabib <jorgehabib.khouri@gmail.com>
2020-09-20 01:22:18 -0300

    Feat: account summary cards added

---
+++ src/pages/Dashboard/MainContent/AccountSummary/styles.ts
@@ -0,0 +1,130 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+
+export const Container = styled.section`
+${({ theme }) => css`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  column-gap: 2.4rem;
+
+  max-height: 17.6rem;
+  height: 100%;
+
+  margin: 2.4rem 0;
+`}
+`;
+
+export const Card = styled.article`
+${({ theme }) => css`
+  background: ${theme.colors.background};
+  max-width: 26.4rem;
+  max-height: 17.6rem;
+
+  width: 100%;
+  height: 100%;
+
+  border-radius: ${theme.radius.default};
+
+  padding: 1.6rem 2.4rem;
+
+  box-shadow: ${theme.shadows.default};
+
+  display: flex;
+  flex-direction: column;
+`}
+`;
+
+export const Header = styled.div<{ iconStroke?: boolean }>`
+${({ theme, iconStroke }) => css`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 0.8rem;
+
+  > h3 {
+    font-size: ${theme.fontSizes.small};
+    color: ${theme.colors.darkGrey};
+    font-weight: 500;
+  }
+
+  > svg {
+    margin-right: 0.8rem;
+    stroke: ${iconStroke && theme.colors.primary};
+
+    width: auto;
+    height: 1.6rem;
+  }
+
+  > button {
+    margin: -0.8rem -1.6rem 0 auto;
+    padding: 0.8rem 1.6rem;
+
+    svg {
+      stroke-width: 1.5;
+      stroke: ${theme.colors.grey};
+    }
+  }
+`}
+`;
+
+export const DataWrapper = styled.div`
+${({ theme }) => css`
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  margin-top: auto;
+  flex: 1;
+`}
+`;
+
+export const LeftData = styled.div`
+${({ theme }) => css`
+  margin-right: 0.8rem;
+  max-width: 55%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 10.4rem;
+    width: auto;
+  }
+`}
+`;
+
+export const RightData = styled.div`
+${({ theme }) => css`
+  font-size: ${theme.fontSizes.small};
+
+  > span:not(:only-child):last-of-type {
+    margin-top: 0.8rem;
+    display: block;
+  }
+`}
+`;
+
+type DataValueType = {
+  income?: boolean;
+  outcome?: boolean;
+}
+
+const getDataColor = (theme: DefaultTheme, { income, outcome }: DataValueType): string => {
+  if (income) {
+    return theme.colors.success;
+  }
+
+  if (outcome) {
+    return theme.colors.error;
+  }
+
+  return theme.colors.darkGrey;
+}
+
+export const DataValue = styled.div<DataValueType>`
+${({ theme, income, outcome }) => css`
+  color: ${getDataColor(theme, { income, outcome })};
+  font-size: ${theme.fontSizes.default};
+`}
+`;
