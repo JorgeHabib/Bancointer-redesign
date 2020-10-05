38f5c6b3c412dff5273de8dff96eab89123dd522
JorgeHabib <jorgehabib.khouri@gmail.com>
2020-10-05 11:19:57 -0300

    add: added new chart options

---
+++ src/resources/line.chart.off.data.ts
@@ -0,0 +1,25 @@
+const lineChartOffData = [
+  {
+    "id": "japan",
+    "data": [
+      {
+        "x": "jan",
+        "y": 0
+      },
+      {
+        "x": "fev",
+        "y": 0
+      },
+      {
+        "x": "mar",
+        "y": 0
+      },
+      {
+        "x": "abr",
+        "y": 0
+      },
+    ]
+  },
+]
+
+export default lineChartOffData;
