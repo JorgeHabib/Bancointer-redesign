baf9f322d6421040b2a099bc70285e8ff6826208
JorgeHabib <jorgehabib.khouri@gmail.com>
2020-10-05 11:18:35 -0300

    fix: fixed global font size

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,7 +31,6 @@ export default createGlobalStyle`
 
   body, input, button {
     font: 1.6rem 'Lato', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    font-size: 16px;
   }
 
   a {
