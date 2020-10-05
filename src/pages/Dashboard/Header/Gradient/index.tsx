c30ae894ddd7bd6d9ae5002ec3a34cc9675e7f4b
JorgeHabib <jorgehabib.khouri@gmail.com>
2020-10-05 11:25:07 -0300

    task: added gradient animation

--- src/pages/Dashboard/Header/Gradient/index.tsx
+++ src/pages/Dashboard/Header/Gradient/index.tsx
@@ -2,9 +2,22 @@ import React, { memo } from 'react';
 
 import { Container } from './styles';
 
+const animation = {
+  unMounted: { width: '0%' },
+  mounted: {
+    width: '100%',
+  }
+}
+
 const Gradient: React.FC = () => {
   return (
-    <Container />
+    <Container
+      layout
+      variants={animation}
+      initial="unMounted"
+      animate="mounted"
+      transition={{ duration: 1.8, delay: 0.5, ease: 'easeInOut' }}
+    />
   );
 };
 
